46eb0c30a0a92b3c9dab0dc5114250cdc2abe309
devallever <devallever@163.com>
2022-10-19 19:43:09 +0800

    FEATURE: 完善自动滚动的RecyclerView

--- ui/src/main/java/app/allever/android/lib/demo/ui/UIMainActivity.kt
+++ ui/src/main/java/app/allever/android/lib/demo/ui/UIMainActivity.kt
@@ -1,13 +1,18 @@
 package app.allever.android.lib.demo.ui
 
 import android.view.ViewOutlineProvider
+import androidx.recyclerview.widget.LinearLayoutManager
 import app.allever.android.lib.common.BaseActivity
 import app.allever.android.lib.core.helper.ActivityHelper
+import app.allever.android.lib.core.helper.DisplayHelper
 import app.allever.android.lib.demo.BR
 import app.allever.android.lib.demo.R
 import app.allever.android.lib.demo.databinding.ActivityUiMainBinding
+import app.allever.android.lib.demo.databinding.RvItemTextBinding
 import app.allever.android.lib.mvvm.base.BaseViewModel
 import app.allever.android.lib.mvvm.base.MvvmConfig
+import com.chad.library.adapter.base.BaseQuickAdapter
+import com.chad.library.adapter.base.viewholder.BaseDataBindingHolder
 import eightbitlab.com.blurview.RenderScriptBlur
 
 class UIMainActivity : BaseActivity<ActivityUiMainBinding, UIMainViewModel>() {
@@ -27,6 +32,22 @@ class UIMainActivity : BaseActivity<ActivityUiMainBinding, UIMainViewModel>() {
         binding.blurBg.setOnClickListener {
             ActivityHelper.startActivity<BlurActivity>()
         }
+
+        val listData = mutableListOf<String>()
+        for(i in 0..10) {
+            listData.add("主播你好啵${i+1}")
+        }
+        val autoScrollAdapter = AutoScrollAdapter()
+        binding.autoScrollRecyclerView.layoutManager = LinearLayoutManager(this)
+        binding.autoScrollRecyclerView.adapter = autoScrollAdapter
+        autoScrollAdapter.data = listData
+        binding.autoScrollRecyclerView.setScrollOffset(DisplayHelper.dip2px(48))
+        binding.autoScrollRecyclerView.start()
+    }
+
+    override fun onDestroy() {
+        super.onDestroy()
+        binding.autoScrollRecyclerView.stop()
     }
 
     override fun getContentMvvmConfig() = MvvmConfig(R.layout.activity_ui_main, BR.uiMainVM)
@@ -35,5 +56,27 @@ class UIMainActivity : BaseActivity<ActivityUiMainBinding, UIMainViewModel>() {
 class UIMainViewModel : BaseViewModel() {
     override fun init() {
 
+    }
+}
+
+class AutoScrollAdapter :
+    BaseQuickAdapter<String, BaseDataBindingHolder<RvItemTextBinding>>(R.layout.rv_item_text) {
+    override fun convert(holder: BaseDataBindingHolder<RvItemTextBinding>, item: String) {
+        val binding = holder.dataBinding ?: return
+        binding.tvText.text = item
+    }
+
+    override fun getItem(position: Int): String {
+        val newPosition = position % data.size
+        return data[newPosition]
+    }
+
+    override fun getItemViewType(position: Int): Int {
+        val realPosition = position % data.size
+        return super.getItemViewType(realPosition)
+    }
+
+    override fun getItemCount(): Int {
+        return Integer.MAX_VALUE
     }
 }
--- ui/src/main/java/app/allever/android/lib/demo/ui/widget/AutoScrollRecyclerView.kt
+++ ui/src/main/java/app/allever/android/lib/demo/ui/widget/AutoScrollRecyclerView.kt
@@ -4,39 +4,24 @@ import android.content.Context
 import android.util.AttributeSet
 import android.view.MotionEvent
 import androidx.recyclerview.widget.RecyclerView
+import app.allever.android.lib.core.function.work.TimerTask2
 import app.allever.android.lib.core.helper.DisplayHelper
-import coil.request.Disposable
-import java.util.concurrent.TimeUnit
 
 /**
  * https://blog.csdn.net/zhanghuaiwang/article/details/123223178
  */
 class AutoScrollRecyclerView(context: Context, attrs: AttributeSet?) : RecyclerView(context, attrs) {
-    private var mAutoTask: Disposable? = null
+    private var mAutoTask: TimerTask2 =  TimerTask2(lifecycleOwner = null, 1000, true) {
+        smoothScrollBy(0, mScrollOffset)
+    }
     private var mScrollOffset = DisplayHelper.dip2px(20)
+
     fun start() {
-        if (mAutoTask != null && !mAutoTask!!.isDisposed) {
-            mAutoTask?.dispose()
-        }
-        /**
-         * @param initialDelay 第一次执行的延迟时间
-         * @param period 每次执行的间隔的时间
-         * @param unit 时间单位
-         * @param scheduler 线程调度器
-         * @return Observable对象
-         */
-        mAutoTask = Observable.interval(3000, 1000, TimeUnit.MILLISECONDS)
-            .observeOn(AndroidSchedulers.mainThread())
-            .subscribe { //垂直自动滑动y轴40
-                smoothScrollBy(0, mScrollOffset)
-            }
+        mAutoTask.start()
     }
 
     fun stop() {
-        if (mAutoTask != null &&!mAutoTask!!.isDisposed) {
-            mAutoTask?.dispose()
-            mAutoTask = null
-        }
+        mAutoTask.cancel()
     }
 
     fun setScrollOffset(value: Int) {
