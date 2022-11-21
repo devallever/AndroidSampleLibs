f33dd0794f9ef46db80fad187bdf944eaa8ec98b
devallever <devallever@163.com>
2022-11-21 21:49:06 +0800

    FEATURE: 新增SmartRefreshLayout方式到回弹效果

--- ui/src/main/java/app/allever/android/lib/demo/ui/FlingScrollTabActivity.kt
+++ ui/src/main/java/app/allever/android/lib/demo/ui/FlingScrollTabActivity.kt
@@ -6,16 +6,18 @@ import app.allever.android.lib.common.TabViewModel
 import app.allever.android.lib.common.databinding.ActivityTabBinding
 import app.allever.android.lib.demo.ui.rebound.ReboundScrollViewFragment
 import app.allever.android.lib.demo.ui.rebound.ReboundViewDragHelperFragment
+import app.allever.android.lib.demo.ui.rebound.SmartRefreshReboundFragment
 
 class FlingScrollTabActivity : TabActivity<ActivityTabBinding, TabViewModel>() {
     override fun getPageTitle() = "回弹效果"
 
     override fun getTabTitles(): MutableList<String> =
-        mutableListOf("ScrollView方式", "ViewDragHelper方式")
+        mutableListOf("ScrollView方式", "ViewDragHelper方式", "SmartRefreshLayout方式")
 
     override fun getFragments(): MutableList<Fragment> =
         mutableListOf(
             ReboundScrollViewFragment(),
             ReboundViewDragHelperFragment(),
+            SmartRefreshReboundFragment()
         )
 }
