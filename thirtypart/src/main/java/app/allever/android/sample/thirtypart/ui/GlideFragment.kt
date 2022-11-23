419108b3f76cd62be98c16ea10d9dab2fd8ae034
devallever <devallever@163.com>
2022-11-23 19:58:08 +0800

    FEATURE: 优化glide渐变

--- thirtypart/src/main/java/app/allever/android/sample/thirtypart/ui/GlideFragment.kt
+++ thirtypart/src/main/java/app/allever/android/sample/thirtypart/ui/GlideFragment.kt
@@ -19,10 +19,10 @@ class GlideFragment : BaseMvvmFragment<FragmentGlideBinding, BaseViewModel>() {
         val factory = DrawableCrossFadeFactory.Builder().setCrossFadeEnabled(true).build()
 
         Glide.with(this)
-            .load("https://gimg2.baidu.com/image_search/src=http%3A%2F%2Fup.enterdesk.com%2Fedpic_source%2F64%2F4c%2F7b%2F644c7b6c1a42fac7d48b517084bfb02c.jpg&refer=http%3A%2F%2Fup.enterdesk.com&app=2002&size=f9999,10000&q=a80&n=0&g=0n&fmt=auto?sec=1671769083&t=f8bd2fbadf0205c011cc930550599981")
+            .load("https://gimg2.baidu.com/image_search/src=http%3A%2F%2Fpic1.win4000.com%2Fwallpaper%2F2020-07-14%2F5f0d6def76ce8.jpg&refer=http%3A%2F%2Fpic1.win4000.com&app=2002&size=f9999,10000&q=a80&n=0&g=0n&fmt=auto?sec=1671712929&t=a9d6d7ea6749ff3fd7b50612e47ec7bb")
             .transition(withCrossFade(factory))
             .diskCacheStrategy(DiskCacheStrategy.NONE)
-            .placeholder(app.allever.android.lib.common.R.drawable.md_btn_shape)
+            .placeholder(app.allever.android.lib.common.R.drawable.shape_cccccc)
             .into(mBinding.iv1)
 
 //        Glide.with(this).load("").into(mBinding.iv1)
