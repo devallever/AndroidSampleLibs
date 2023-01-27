09b1535d327ed74f96a341bc94c2d3625c61e6e4
devallever <devallever@163.com>
2023-01-27 12:23:24 +0800

    FEATURE: ListFragment添加标题和返回

--- common/src/main/java/app/allever/android/lib/common/ListFragment.kt
+++ common/src/main/java/app/allever/android/lib/common/ListFragment.kt
@@ -1,8 +1,12 @@
 package app.allever.android.lib.common
 
+import android.text.TextUtils
+import android.view.KeyEvent
 import androidx.recyclerview.widget.LinearLayoutManager
 import androidx.viewbinding.ViewBinding
 import app.allever.android.lib.common.databinding.FragmentListBinding
+import app.allever.android.lib.core.helper.DisplayHelper
+import app.allever.android.lib.core.helper.ViewHelper
 import app.allever.android.lib.mvvm.base.BaseViewModel
 import com.chad.library.adapter.base.BaseQuickAdapter
 
@@ -14,6 +18,8 @@ abstract class ListFragment<DB : ViewBinding, VM : BaseViewModel, T> :
     override fun inflate() = FragmentListBinding.inflate(layoutInflater)
 
     override fun init() {
+
+        initTopBar()
 
         mBinding.smartRefreshLayout.setEnableOverScrollDrag(true)
 
@@ -29,10 +35,32 @@ abstract class ListFragment<DB : ViewBinding, VM : BaseViewModel, T> :
         }
     }
 
+    private fun initTopBar() {
+        val title = getTitle()
+        val showTopBar = !TextUtils.isEmpty(title)
+        if (showTopBar) {
+            ViewHelper.setViewHeight(
+                mBinding.statusBar,
+                DisplayHelper.getStatusBarHeight(requireContext())
+            )
+            ViewHelper.setVisible(mBinding.topBar, showTopBar)
+            mBinding.tvTitle.text = title
+            mBinding.ivBack.setOnClickListener {
+                val runtime = Runtime.getRuntime()
+                runtime.exec("input keyevent " + KeyEvent.KEYCODE_BACK)
+
+            }
+        }
+    }
+
     abstract fun getAdapter(): BaseQuickAdapter<T, *>
     abstract fun getList(): MutableList<T>
     open protected fun onItemClick(position: Int, item: T) {
 
+    }
+
+    open fun getTitle(): String {
+        return ""
     }
 }
 
