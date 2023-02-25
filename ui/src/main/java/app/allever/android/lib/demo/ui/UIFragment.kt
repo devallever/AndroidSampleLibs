d5ee24429e9e765a98a5bd45c77f18ac2e04ecec
devallever <devallever@163.com>
2023-02-25 11:25:39 +0800

    FEATURE-UI: 添加自动换行标签布局用法

--- ui/src/main/java/app/allever/android/lib/demo/ui/UIFragment.kt
+++ ui/src/main/java/app/allever/android/lib/demo/ui/UIFragment.kt
@@ -1,12 +1,17 @@
 package app.allever.android.lib.demo.ui
 
+import android.content.Context
+import android.view.View
 import android.view.ViewOutlineProvider
+import android.widget.TextView
 import androidx.recyclerview.widget.LinearLayoutManager
 import app.allever.android.lib.common.BaseFragment
+import app.allever.android.lib.core.ext.toast
 import app.allever.android.lib.core.helper.ActivityHelper
 import app.allever.android.lib.core.helper.DisplayHelper
 import app.allever.android.lib.demo.R
 import app.allever.android.lib.demo.databinding.FragmentUiBinding
+import app.allever.android.lib.demo.ui.autolayout.AutoAdapter
 import app.allever.android.lib.demo.ui.widget.OverlapManager
 import app.allever.android.lib.demo.util.TextAndPictureUtil
 import app.allever.android.lib.mvvm.base.BaseViewModel
@@ -63,7 +68,30 @@ class UIFragment : BaseFragment<FragmentUiBinding, UIViewModel>() {
                 override fun convert(holder: BaseViewHolder, item: String) {}
             }
 
+        //自动换行标签布局
+       initAutoLayout()
 
+
+    }
+
+    private fun initAutoLayout() {
+        val list = ArrayList<String>()
+        list.add("3333")
+        list.add("1")
+        list.add("22222222222222222222222222222222222222222222")
+        list.add("清空万里")
+        list.add("两条相交线")
+        list.add("短途")
+        list.add("希望在明天会更好")
+
+        val autoAdapter = MyAutoAdapter(requireContext())
+        autoAdapter.setOnItemClickListener(object : MyAutoAdapter.OnItemClickListener {
+            override fun onItemClick(view: View?, text: String?) {
+                toast(text)
+            }
+        })
+        autoAdapter.setData(list)
+        mBinding.autoLayout.setAdapter(autoAdapter)
     }
 
     override fun onDestroy() {
@@ -77,6 +105,47 @@ class UIFragment : BaseFragment<FragmentUiBinding, UIViewModel>() {
 class UIViewModel : BaseViewModel() {
     override fun init() {
 
+    }
+}
+
+class MyAutoAdapter(private val mContext: Context) : AutoAdapter() {
+    private val mList: MutableList<String> = ArrayList()
+    private var mListener: OnItemClickListener? = null
+    fun setData(list: ArrayList<String>?) {
+        mList.clear()
+        mList.addAll(list!!)
+        notifyDataSetChanged()
+    }
+
+    override fun getCount(): Int {
+         return mList.size
+    }
+
+
+    override fun getItem(position: Int): Any? {
+        return null
+    }
+
+    override fun getItemId(position: Int): Long {
+        return 0
+    }
+
+    override fun getView(position: Int): View {
+        val view = View.inflate(mContext, R.layout.item_tag, null)
+        val tv = view.findViewById<View>(R.id.tv) as TextView
+        tv.text = mList[position]
+        if (mListener != null) {
+            tv.setOnClickListener { v -> mListener!!.onItemClick(v, mList[position]) }
+        }
+        return view
+    }
+
+    fun setOnItemClickListener(listener: OnItemClickListener?) {
+        mListener = listener
+    }
+
+    interface OnItemClickListener {
+        fun onItemClick(view: View?, text: String?)
     }
 }
 
