b51148b6ed097e2cb806128b032ab70ba9c45891
devallever <devallever@163.com>
2023-03-09 14:00:46 +0800

    FEATURE-AUDIO-VIDEO: MediaPlayer播放器内核

--- audiovideo/src/main/java/app/allever/android/learning/audiovideo/kernel/IJKPlayer.kt
+++ audiovideo/src/main/java/app/allever/android/learning/audiovideo/kernel/IJKPlayer.kt
@@ -1,24 +1,99 @@
 package app.allever.android.learning.audiovideo.kernel
 
-import app.allever.android.lib.core.ext.toast
+import android.content.res.AssetFileDescriptor
+import android.view.Surface
+import android.view.SurfaceHolder
 
 /***
  * IJKPlayer内核
  */
-class IJKPlayer: AbsPlayer() {
-    override fun play() {
-        toast("IJKPlayer内核：play")
+class IJKPlayer : AbsPlayer() {
+
+    override fun initPlayer() {
+
+    }
+
+    override fun setDataSource(path: String, headers: Map<String, String>?) {
+
+    }
+
+    override fun setDataSource(accessFileDescriptor: AssetFileDescriptor) {
+
+    }
+
+    override fun setSurface(surface: Surface?) {
+
+    }
+
+    override fun prepareAsync() {
+
+    }
+
+    override fun start() {
+
     }
 
     override fun pause() {
-        toast("IJKPlayer内核：pause")
+
     }
 
     override fun stop() {
-        toast("IJKPlayer内核：stop")
+
+    }
+
+    override fun reset() {
+
+    }
+
+    override fun isPlaying(): Boolean {
+        return false
+    }
+
+    override fun seekTo(time: Long) {
+
     }
 
     override fun release() {
-        toast("IJKPlayer内核：release")
+
+    }
+
+    override fun getCurrentPosition(): Long {
+        return 0
+    }
+
+    override fun getDuration(): Long {
+        return 0
+    }
+
+    override fun getBufferedPercentage(): Int {
+        return 0
+    }
+
+    override fun setDisplay(holder: SurfaceHolder?) {
+
+    }
+
+    override fun setVolume(v1: Float, v2: Float) {
+
+    }
+
+    override fun setLooping(isLooping: Boolean) {
+
+    }
+
+    override fun setOptions() {
+
+    }
+
+    override fun setSpeed(speed: Float) {
+
+    }
+
+    override fun getSpeed(): Float {
+        return 0F
+    }
+
+    override fun getTcpSpeed(): Long {
+        return 0
     }
 }
