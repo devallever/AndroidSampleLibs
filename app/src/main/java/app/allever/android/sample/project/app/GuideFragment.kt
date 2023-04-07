8cb2b75fd00445c09c630d8768d9ca64312db533
devallever <devallever@163.com>
2023-04-07 15:52:43 +0800

    提交代码

--- app/src/main/java/app/allever/android/sample/project/app/GuideFragment.kt
+++ app/src/main/java/app/allever/android/sample/project/app/GuideFragment.kt
@@ -22,12 +22,12 @@ class GuideFragment : BaseFragment<FragmentGuideBinding, BaseViewModel>() {
 
     private fun initData() {
         val list = mutableListOf<GuideItem>()
-        val title = "GIF Memes"
+        val title = "Device Detector"
         list.add(
             GuideItem(
                 R.color.googleBleu,
                 title,
-                "The hot gif memes",
+                "Device Fonder Tools",
                 app.allever.android.sample.project.R.drawable.p01
             )
         )
@@ -35,7 +35,7 @@ class GuideFragment : BaseFragment<FragmentGuideBinding, BaseViewModel>() {
             GuideItem(
                 R.color.googleBleu,
                 title,
-                "Search all gif memes",
+                "Bluetooth device finder",
                 app.allever.android.sample.project.R.drawable.p02
             )
         )
@@ -43,7 +43,7 @@ class GuideFragment : BaseFragment<FragmentGuideBinding, BaseViewModel>() {
             GuideItem(
                 R.color.googleBleu,
                 title,
-                "Collect favorite gif memes",
+                "Device distance",
                 app.allever.android.sample.project.R.drawable.p03
             )
         )
@@ -51,34 +51,7 @@ class GuideFragment : BaseFragment<FragmentGuideBinding, BaseViewModel>() {
             GuideItem(
                 R.color.googleBleu,
                 title,
-                "Share the gif memes",
-                app.allever.android.sample.project.R.drawable.p06
-            )
-        )
-
-        list.add(
-            GuideItem(
-                R.color.googleBleu,
-                title,
-                "Save gif memes local",
-                app.allever.android.sample.project.R.drawable.p07
-            )
-        )
-
-        list.add(
-            GuideItem(
-                R.color.googleBleu,
-                title,
-                "Video to Gif Memes",
-                app.allever.android.sample.project.R.drawable.p05
-            )
-        )
-
-        list.add(
-            GuideItem(
-                R.color.googleBleu,
-                title,
-                "Manage your Gif Memes",
+                "Metal detector",
                 app.allever.android.sample.project.R.drawable.p04
             )
         )
