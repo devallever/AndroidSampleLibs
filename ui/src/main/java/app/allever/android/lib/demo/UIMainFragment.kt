8e5e4d9110a35af8b5a23fa4694842bce8e3072e
devallever <devallever@163.com>
2023-07-23 15:56:53 +0800

    FEATURE: 优化UI交互效果代码

--- ui/src/main/java/app/allever/android/lib/demo/UIMainFragment.kt
+++ ui/src/main/java/app/allever/android/lib/demo/UIMainFragment.kt
@@ -1,9 +1,12 @@
 package app.allever.android.lib.demo
 
+import androidx.recyclerview.widget.GridLayoutManager
+import androidx.recyclerview.widget.RecyclerView
 import app.allever.android.lib.common.FragmentActivity
 import app.allever.android.lib.common.ListFragment
 import app.allever.android.lib.common.ListViewModel
-import app.allever.android.lib.common.adapter.TextAdapter
+import app.allever.android.lib.common.adapter.TextClickAdapter
+import app.allever.android.lib.common.adapter.bean.TextClickItem
 import app.allever.android.lib.common.databinding.FragmentListBinding
 import app.allever.android.lib.core.helper.ActivityHelper
 import app.allever.android.lib.demo.ui.*
@@ -13,64 +16,51 @@ import app.allever.android.lib.demo.ui.dragclose.DragCloseMainFragment
 import app.allever.android.lib.demo.ui.expandrecycler.ContactExpandListFragment
 import app.allever.android.lib.demo.ui.notification.NotificationMainFragment
 import app.allever.android.lib.demo.ui.sticktop.StickyTopMainFragment
-import app.allever.android.lib.widget.fragment.EmptyFragment
 import com.chad.library.adapter.base.BaseQuickAdapter
 
-class UIMainFragment : ListFragment<FragmentListBinding, ListViewModel, String>() {
-    override fun getAdapter(): BaseQuickAdapter<String, *> = TextAdapter()
+class UIMainFragment : ListFragment<FragmentListBinding, ListViewModel, TextClickItem>() {
 
-    override fun getList() =
-        mutableListOf(
-            "UI效果",
-            "吸顶效果",
-            "个人主页折叠效果",
-            "回弹效果",
-            "自动滚动效果",
-            "探探-交互效果",
-            "Soul-交互效果",
-            "分组联系人列表",
-            "弹窗",
-            "通知",
-            "拖拽关闭页面效果",
-            "分组联系人列表-折叠"
+    override fun getAdapter(): BaseQuickAdapter<TextClickItem, *> = TextClickAdapter()
+
+    override fun getList() = mutableListOf(
+        TextClickItem("UI效果") {
+            FragmentActivity.start<UIFragment>(it.title)
+        },
+        TextClickItem("吸顶效果") {
+            FragmentActivity.start<StickyTopMainFragment>(it.title)
+        },
+        TextClickItem("个人主页折叠效果") {
+            ActivityHelper.startActivity<UserCenterActivity>()
+        },
+        TextClickItem("回弹效果") {
+            FragmentActivity.start<FlingScrollTabFragment>(it.title)
+        },
+        TextClickItem("自动滚动效果") {
+            FragmentActivity.start<AutoScrollMainFragment>(it.title)
+        },
+        TextClickItem("探探-交互效果") {
+            FragmentActivity.start<TanTanFragment>(it.title)
+        },
+        TextClickItem("Soul-交互效果") {
+            FragmentActivity.start<SoulFragment>(it.title)
+        },
+        TextClickItem("分组联系人列表") {
+            FragmentActivity.start<ContactListFragment>(it.title)
+        },
+        TextClickItem("分组联系人列表-折叠") {
+            FragmentActivity.start<ContactExpandListFragment>(it.title)
+        },
+        TextClickItem("弹窗") {
+            FragmentActivity.start<DialogMainFragment>(it.title)
+        },
+        TextClickItem("通知") {
+            FragmentActivity.start<NotificationMainFragment>(it.title)
+        },
+        TextClickItem("拖拽关闭页面效果") {
+            FragmentActivity.start<DragCloseMainFragment>(it.title)
+        },
+
+
+
         )
-
-    override fun onItemClick(position: Int, item: String) {
-        when (position) {
-            0 -> {
-                FragmentActivity.start<UIFragment>(item)
-            }
-            1 -> {
-                FragmentActivity.start<StickyTopMainFragment>(item)
-            }
-            2 -> {
-                ActivityHelper.startActivity<UserCenterActivity>()
-            }
-            3 -> {
-                FragmentActivity.start<FlingScrollTabFragment>(item)
-            }
-            4 -> {
-                FragmentActivity.start<AutoScrollMainFragment>(item)
-            }
-            5 -> {
-                FragmentActivity.start<TanTanFragment>(item)
-            }
-            6 -> {
-                FragmentActivity.start<SoulFragment>(item)
-            }
-            7 -> {
-                FragmentActivity.start<ContactListFragment>(item)
-            }
-            8 -> {
-                FragmentActivity.start<DialogMainFragment>(item)
-            }
-            9 -> FragmentActivity.start<NotificationMainFragment>(item)
-            10 -> {
-                FragmentActivity.start<DragCloseMainFragment>(item)
-            }
-            11 -> {
-                FragmentActivity.start<ContactExpandListFragment>(item)
-            }
-        }
-    }
 }
